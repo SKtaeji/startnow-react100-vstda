2c3da293d828590e9997d3bf9d9422befd2b7d0f
skim <caelum.modus@gmail.com>
2018-01-12 19:19:57 -0800

    Initialized project

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="container-fluid">
+          
+          <header className="App-header">
+            <h1 className="App-title">Very Simple ToDo App</h1>
+            <h5 className="App-subtitle">Never forget anything again.</h5>
+            <hr />
+          </header>
+          
+          <div className="row">
+            <div className="col-lg-4">
+              
+            </div>
+            <div className="col-lg-8"></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
